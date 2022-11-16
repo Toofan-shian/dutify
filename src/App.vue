6698d4a356ef9a33c54cd386bb5399346c13fee0
Toofan <toofan.sh.92@gmail.com>
2022-11-16 13:16:44 +0330

    toolbar-added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
   <v-app>
-    
+    <navbar/>
+
+    <v-main class="blue">
+
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import navbar from '@/components/toolbar.vue'
 
 export default {
   name: 'App',
+  components: {
+    navbar
+  },
 
   data: () => ({
-    //
+
   }),
 };
 </script>
+
+<style>
+
+</style>
---
+++ src/components/toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <v-app-bar class="red" app>
+            <v-app-bar-title>
+                Dutify
+            </v-app-bar-title>
+            <v-spacer></v-spacer>
+            <v-btn>
+                <span>Sign Out</span>
+                <v-icon right>
+                    mdi-exit-to-app
+                </v-icon>
+            </v-btn>
+        </v-app-bar>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
