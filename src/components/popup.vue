4c9ce4bfebe7f1bee3950988b2187628989fd648
Toofan <toofan.shian@gmail.com>
2022-11-22 01:32:04 +0330

    add form validation

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -22,19 +22,39 @@
           </v-card-title>
   
           <v-card-text>
-            <v-form>
+            <v-form ref="form">
                 <v-text-field
                 label="Title"
                 prepend-inner-icon="mdi-folder"
                 v-model="title"
+                :rules="formRules"
                 ></v-text-field>
 
                 <v-textarea
                 label="info"
+                :rules="formRules"
                 prepend-inner-icon="mdi-pencil"
                 v-model="info"
                 >
                 </v-textarea>
+
+                <v-menu>
+                    <template v-slot:activator="{on}">
+                        <v-text-field
+                        label="Due By"
+                        v-on="on"
+                        prepend-inner-icon="mdi-calendar-range"
+                        :value="date"
+                        >
+                        </v-text-field>
+                    </template>
+
+                    <v-date-picker
+                    v-model="date"
+                    ></v-date-picker>
+
+                </v-menu>
+
                 <v-btn class="success mx-0" @click="atSubmit">Add Project</v-btn>
             </v-form>
           </v-card-text>
@@ -46,18 +66,29 @@
   </template>
 
   <script>
+
+
   export default {
     data () {
       return {
         dialog: false,
         title: '',
-        info: ''
+        info: '',
+        date: null,
+        formRules: [
+            v => v.length >= 3 || 'at least 3 charecter'
+        ]
       }
     },
     methods: {
         atSubmit() {
-            console.log(this.title, this.info)
+            if (this.$refs.form.validate()){
+                console.log(this.title, this.info)
+            }
         }
+    },
+    computed: {
+
     }
   }
 </script>
